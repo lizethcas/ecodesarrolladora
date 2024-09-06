0bc41d2d47a9f1d0e83f0fdd0727af663386e51c
Lizeth castillo <64451121+lizethcas@users.noreply.github.com>
2024-09-05 19:09:37 -0500

    clean app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,63 +2,10 @@ import { useEffect, useState } from "react";
 import Button from "./components/Atoms/ButtonComponent/Button.component";
 import axios from "axios";
 
-interface UserCredentials {
-  username: string;
-  email: string;
-  password: string;
-}
 
-interface ApiResponse {
-  // Define aquí la estructura de la respuesta que esperas de la API
-  message: string;
-  data: string; // Cambia 'string' por el tipo específico si conoces la estructura exacta
-}
 
 function App() {
-  // Estado para almacenar los datos de la API y errores
-  const [data, setData] = useState<ApiResponse | null>(null);
-  const [error, setError] = useState<string | null>(null);
-
-  // Credenciales de ejemplo para el login
-  const userCredentials: UserCredentials = {
-    username: "johndoe",
-    email: "johndoe@example.com",
-    password: "securepassword123",
-  };
-
-  // useEffect para llamar a la API cuando se carga el componente
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // Realiza la solicitud POST a la API usando las credenciales de usuario
-        const response = await axios.post<ApiResponse>(
-          "http://localhost:8000/api/signin/", // URL de tu API
-          userCredentials, // Enviar las credenciales
-          {
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
-
-        console.log("Coliving creado:", response.data);
-        setData(response.data);
-        setError(null); // Limpiar errores si la solicitud es exitosa
-      } catch (error: any) {
-        setData(null);
-        setError(
-          error.response?.data?.message || "Error al conectar con la API"
-        );
-        console.log(error)
-        console.log(error.response.data)
-      }
-    };
-
-    // Llamada a la función fetchData
-    fetchData();
-  }, []); // Array vacío para ejecutar solo una vez al montar el componente
-
-  return (
+   return (
     <>
       <h1 className="text-3xl">Titulo</h1>
       <Button
@@ -69,15 +16,7 @@ function App() {
         }}
       />
 
-      {/* Mostrar los datos de la API si están disponibles */}
-      {data ? (
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      ) : error ? (
-        <p>Error: {error}</p>
-      ) : (
-        <p>Cargando datos...</p>
-      )}
-    </>
+         </>
   );
 }
 
